79c9a7ddd0d7c73c3577448bee6c5208129ea2b7
Andreas Werdich <andreas_werdich@hms.harvard.edu>
2025-10-10 07:02:55 -0400

    Python package

--- src/llmtools/__init__.py
+++ src/llmtools/__init__.py
@@ -1,2 +1,8 @@
+from pathlib import Path
+
+# you can use os.path and open() as well
+__version__ = Path(__file__).parent.joinpath("VERSION").read_text()
+__authors__ = Path(__file__).parent.joinpath("AUTHORS").read_text()
+
 def main() -> None:
-    print("Hello from llmtools!")
+    print("Hello from LLM-TOOLS-HMS!")
